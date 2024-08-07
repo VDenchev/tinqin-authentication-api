0faeea0dac022e8d63cb97c6007bb46da39deca8
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-08 00:14:59 +0300

    [TA-7] Create input and output POJOs for register operation

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/register/input/RegisterInput.java
@@ -0,0 +1,46 @@
+package com.tinqinacademy.authentication.api.operations.register.input;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class RegisterInput {
+
+  @NotBlank(message = "Username must not be blank")
+  @Size(min = 2, message = "Username must be at least 2 characters in length")
+  @Schema(example = "ivan120")
+  private String username;
+
+  @NotBlank(message = "Password must not be blank")
+  @Size(min = 6, message = "Password must be at least 6 characters in length")
+  @Schema(example = "ivan123456")
+  private String password;
+
+  @NotBlank(message = "First name must not be blank")
+  @Size(min = 2, max = 40, message = "First name must be between 2 and 40 characters long")
+  @Schema(example = "Ivan")
+  private String firstName;
+
+  @NotBlank(message = "Last name must not be blank")
+  @Size(min = 2, max = 40, message = "Last name must be between 2 and 40 characters long")
+  @Schema(example = "Petrov")
+  private String lastName;
+
+  @NotBlank(message = "Email cannot be blank")
+  @Email(message = "Invalid email format", regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+  @Schema(example = "ivan.petrov@example.com")
+  private String email;
+}
