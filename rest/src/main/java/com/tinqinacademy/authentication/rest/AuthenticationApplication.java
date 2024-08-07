a0095819660451a93d3fdc7f8d88a3206414faee
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-07 13:02:18 +0300

    [TA-3] Create jpa entities and repositories (#4)
    * [TA-3] Add jpa entities
    
    * [TA-3] Create repositories

--- rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
@@ -2,8 +2,12 @@ package com.tinqinacademy.authentication.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tinqinacademy.authentication")
+@EntityScan(basePackages = "com.tinqinacademy.authentication.persistence.entities")
+@EnableJpaRepositories(basePackages = "com.tinqinacademy.authentication.persistence.repositories")
 public class AuthenticationApplication {
 
   public static void main(String[] args) {
