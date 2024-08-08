dadbb943c42f4ec55f66bfd7083d00c7581a6fdd
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-08 23:49:46 +0300

    [TA-13] Add an object mapper config

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/config/JacksonConfig.java
@@ -0,0 +1,30 @@
+package com.tinqinacademy.authentication.api.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+    mapper.registerModule(javaTimeModule);
+
+    mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+    return mapper;
+  }
+}
