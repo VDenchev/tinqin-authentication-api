8f344a44a6b64ef96732f48501e79a45aecd2c96
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-09 21:29:49 +0300

    [TA-15] Create a service which sends out and saves the verification code

---
+++ core/src/main/java/com/tinqinacademy/authentication/core/services/EmailConfirmationService.java
@@ -0,0 +1,45 @@
+package com.tinqinacademy.authentication.core.services;
+
+import com.tinqinacademy.authentication.persistence.entities.User;
+import com.tinqinacademy.authentication.persistence.entities.VerificationCode;
+import com.tinqinacademy.authentication.persistence.mongorepositories.VerificationCodeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailConfirmationService {
+
+  private final MailService mailService;
+  private final VerificationCodeRepository verificationCodeRepository;
+
+  @Async
+  public void sendConfirmationEmail(User user) {
+
+    String code = generateCode();
+    mailService.sendConfirmationEmail(user.getEmail(), code);
+
+    VerificationCode verificationCode = VerificationCode.builder()
+        .code(code)
+        .userId(user.getId())
+        .createdAt(Instant.now())
+        .build();
+
+    verificationCodeRepository.save(verificationCode);
+  }
+
+  private String generateCode() {
+    SecureRandom random = new SecureRandom();
+
+    return random.ints(6, 0, 10)
+        .mapToObj(Integer::toString)
+        .reduce((a, b) -> a + b)
+        .get();
+  }
+}
