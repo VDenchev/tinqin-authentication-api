7b1577ffe5974999588d3a1e037b578278d27f73
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-11 22:44:34 +0300

    [TA-21] Change exception constants

--- api/src/main/java/com/tinqinacademy/authentication/api/constants/ExceptionMessages.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/constants/ExceptionMessages.java
@@ -3,12 +3,13 @@ package com.tinqinacademy.authentication.api.constants;
 public class ExceptionMessages {
 
   public static final String NOT_FOUND_FORMAT = "Entity of type %s with id=%s not found";
-  public static final String INVALID_CODE_FORMAT = "Invalid verification code - %s";
+  public static final String INVALID_VERIFICATION_CODE_MESSAGE = "Invalid verification code";
+  public static final String INVALID_RECOVERY_CODE_MESSAGE = "Invalid recovery code";
   public static final String USERNAME_TAKEN_FORMAT = "User with the username \"%s\" already exists";
   public static final String EMAIL_TAKEN_FORMAT = "User with email \"%s\" already exists";
   public static final String PHONE_NO_TAKEN_FORMAT = "User with phoneNo \"%s\" already exists";
   public static final String UNKNOWN_ROLE_FORMAT = "Role %s does not exist";
   public static final String INVALID_CREDENTIALS_MESSAGE = "Invalid credentials";
-  public static final String EMAIL_NOT_CONFIRMED = "Email is not confirmed";
+  public static final String EMAIL_NOT_CONFIRMED_MESSAGE = "Email is not confirmed";
   public static final String RECOVERY_NOT_REQUESTED_MESSAGE = "No recovery request has been made for this email";
 }
--- api/src/main/java/com/tinqinacademy/authentication/api/exceptions/InvalidCodeException.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/exceptions/InvalidCodeException.java
@@ -1,10 +1,8 @@
 package com.tinqinacademy.authentication.api.exceptions;
 
-import static com.tinqinacademy.authentication.api.constants.ExceptionMessages.INVALID_CODE_FORMAT;
-
 public class InvalidCodeException extends RuntimeException {
 
-  public InvalidCodeException(String code) {
-    super(String.format(INVALID_CODE_FORMAT, code));
+  public InvalidCodeException(String message) {
+    super(message);
   }
 }
