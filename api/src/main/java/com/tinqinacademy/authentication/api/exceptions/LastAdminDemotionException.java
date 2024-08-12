4bba9b85c8ea3585aacabc44cae367ddc7395842
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-12 20:16:30 +0300

    [TA-27] Add custom exception when trying to demote the last admin in the system (might be redundant since you cannot demote yourself)

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/exceptions/LastAdminDemotionException.java
@@ -0,0 +1,8 @@
+package com.tinqinacademy.authentication.api.exceptions;
+
+public class LastAdminDemotionException extends RuntimeException {
+
+  public LastAdminDemotionException(String message) {
+    super(message);
+  }
+}
