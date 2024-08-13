21349616d8d5dc91d07bc79f5afe1d9496d39496
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-13 22:10:32 +0300

    [TA-32] Create input, output pojos and operation interface for the logout feature

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/logout/input/LogoutInput.java
@@ -0,0 +1,17 @@
+package com.tinqinacademy.authentication.api.operations.logout.input;
+
+import com.tinqinacademy.authentication.api.base.OperationInput;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@ToString
+public class LogoutInput implements OperationInput {
+
+}
