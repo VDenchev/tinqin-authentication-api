d74888925870a7bb2c2d98050ab5246f59c5029f
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 11:04:43 +0300

    [TA-36] Add feign client and feign route constants

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/apiroutes/FeignClientApiRoutes.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.authentication.api.apiroutes;
+
+public class FeignClientApiRoutes {
+
+  public static final String VALIDATE_TOKEN = "POST " + RestApiRoutes.VALIDATE_TOKEN;
+}
