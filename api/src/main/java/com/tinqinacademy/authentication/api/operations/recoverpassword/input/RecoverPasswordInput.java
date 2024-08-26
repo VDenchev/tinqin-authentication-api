d778e3f0ffd0fb9f98af2a2b83e92bb48549c933
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-26 13:27:48 +0300

    [TA-46] Change recover password input and processor

--- api/src/main/java/com/tinqinacademy/authentication/api/operations/recoverpassword/input/RecoverPasswordInput.java
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/recoverpassword/input/RecoverPasswordInput.java
@@ -2,8 +2,6 @@ package com.tinqinacademy.authentication.api.operations.recoverpassword.input;
 
 import com.tinqinacademy.authentication.api.base.OperationInput;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,8 +17,6 @@ import lombok.ToString;
 @ToString
 public class RecoverPasswordInput implements OperationInput {
 
-  @NotBlank(message = "Email cannot be blank")
-  @Email(message = "Invalid email format", regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
   @Schema(example = "ivan.petrov@example.com")
   private String email;
 }
